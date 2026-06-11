088658cb2dc9bf98d9fefb3455eae5de0506320e
AzureAD\AliRabbani <ali.rabbani@royalcyber.com>
2026-06-11 17:57:27 +0500

    Finalized the Feedback on all steps

--- cartridges/bm_accelerator/cartridge/scripts/migration/sfcc-credentials.js
+++
@@ -1,7 +0,0 @@
-/* AUTO-GENERATED — do not commit. Run: npm run config:generate */
-'use strict';
-
-module.exports = {
-    "bmUsername": "arsalan.ahmed@royalcyber.com",
-    "bmPassword": "Roy@l@321$#!"
-};
